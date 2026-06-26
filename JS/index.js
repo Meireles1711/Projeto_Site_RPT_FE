f145bab887c1bb5232d5fd65fa240339f0f9f9d5
Aluno <aluno@fiemguba.srv>
2026-06-25 22:16:50 -0300

    Modificado menu, adicionado menu interativo e prototipo de sistema de lista

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+const modal = document.getElementById("modalCadastro");
+
+const btnAbrir = document.getElementById("abrirCadastro");
+const btnFechar = document.getElementById("fecharCadastro");
+
+btnAbrir.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+btnFechar.addEventListener("click", () => {
+    modal.style.display = "none";
+});
