20cb990916fba5c3356aeba627fcd52606339cf2
Pedro0001269 <0001269448@senaimgaluno.com.br>
2026-07-09 21:52:13 -0300

    criada pagina produtos, items,pedidos,imtegrasão com api paginas cidades

--- java/cidades.js
+++ java/cidades.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/formas';
+const API_URL = 'http://localhost:3000/cidades';
 
 
 async function  listar() { 
@@ -8,19 +8,20 @@ async function  listar() {
 
 
     const res = await fetch(API_URL);
-    const formas_p = await res.json();
+    const cidades = await res.json();
 
     const tabela = document.getElementById("tabelaTipos");
     tabela.innerHTML = "";
 
-    formas_p.forEach(tipo=> {
+    cidades.forEach(tipo=> {
         tabela.innerHTML +=`
             <tr>
                 <td>${tipo.id}</td>
-                <td>${tipo.descricao}</td>
+                <td>${tipo.nome}</td>
+                  <td>${tipo.uf}</td>
                 
                  <td>
-                    <button onclick="abrirEditar(${tipo.id},'${tipo.descricao}')">Editar</button>
+                    <button onclick="abrirEditar(${tipo.id},'${tipo.nome}','${tipo.uf}')">Editar</button>
                     <button onclick="deletar(${tipo.id})">Excluir</button>
                 </td>
 
@@ -34,12 +35,13 @@ async function  listar() {
     }
     
     async function criar() {
-        const descricao = document.getElementById("descricaoAdd").value;
+        const nome = document.getElementById("nomeAdd").value;
+        const uf = document.getElementById("ufAdd").value;
        
         await fetch(API_URL,{
             method: "POST" ,
             headers:{"Content-type": "application/json"},
-            body:JSON.stringify({descricao})
+            body:JSON.stringify({nome,uf})
         });
         fecharModal("modalAdicionar");
         listar();
@@ -53,10 +55,11 @@ async function  listar() {
         
     }
    
-    function abrirEditar(id,descricao) {
+    function abrirEditar(id,nome,uf) {
 
         document.getElementById("idEdit").value = id;
-        document.getElementById("descricaoEdit").value = descricao;
+        document.getElementById("nomeEdit").value = nome;
+        document.getElementById("ufEdit").value = uf;
        
     
     
@@ -70,12 +73,13 @@ async function  listar() {
 
         
         const id = document.getElementById("idEdit").value;
-        const descricao = document.getElementById("descricaoEdit").value;
+        const nome = document.getElementById("nomeEdit").value;
+        const uf = document.getElementById("ufEdit").value;
       
         await fetch(`${API_URL}/${id}`,{
             method:"PUT",
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({descricao})
+            body: JSON.stringify({nome,uf})
         });
         fecharModal("modalEditar");
         listar();
@@ -86,7 +90,7 @@ async function  listar() {
 
 
     async function deletar(id) {
-        if (!confirm("Deseja excluir esta Forma de Pagamento ?")) return;
+        if (!confirm("Deseja excluir esta Cidade ?")) return;
         await fetch(`${API_URL}/${id}`,{
             method: "DELETE"
     });
--- java/produtos.js
+++ java/produtos.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/formas';
+const API_URL = 'http://localhost:3000/cidades';
 
 
 async function  listar() { 
@@ -8,19 +8,22 @@ async function  listar() {
 
 
     const res = await fetch(API_URL);
-    const formas_p = await res.json();
+    const cidades = await res.json();
 
     const tabela = document.getElementById("tabelaTipos");
     tabela.innerHTML = "";
 
-    formas_p.forEach(tipo=> {
+    cidades.forEach(tipo=> {
         tabela.innerHTML +=`
             <tr>
                 <td>${tipo.id}</td>
                 <td>${tipo.descricao}</td>
+                  <td>${tipo.unidade}</td>
+                  <td>${tipo.valor_unit}</td>
+                  <td>${tipo.estoque}</td>
                 
                  <td>
-                    <button onclick="abrirEditar(${tipo.id},'${tipo.descricao}')">Editar</button>
+                    <button onclick="abrirEditar(${tipo.id},'${tipo.descricao}','${tipo.unidade}','${tipo.valor_unit}','${tipo.estoque}')">Editar</button>
                     <button onclick="deletar(${tipo.id})">Excluir</button>
                 </td>
 
@@ -35,11 +38,14 @@ async function  listar() {
     
     async function criar() {
         const descricao = document.getElementById("descricaoAdd").value;
+        const unidade= document.getElementById("unidadeAdd").value;
+        const valor_unit = document.getElementById("valorAdd").value;
+        const estoque = document.getElementById("estoqueAdd").value;
        
         await fetch(API_URL,{
             method: "POST" ,
             headers:{"Content-type": "application/json"},
-            body:JSON.stringify({descricao})
+            body:JSON.stringify({descricao,unidade,valor_unit,estoque})
         });
         fecharModal("modalAdicionar");
         listar();
@@ -53,10 +59,13 @@ async function  listar() {
         
     }
    
-    function abrirEditar(id,descricao) {
+    function abrirEditar(id,descricao,unidade,valor_unit,estoque) {
 
         document.getElementById("idEdit").value = id;
         document.getElementById("descricaoEdit").value = descricao;
+        document.getElementById("unidadeEdit").value = unidade;
+        document.getElementById("valorEdit").value =valor_unit;
+        document.getElementById("estoqueEdit").value = estoque;
        
     
     
@@ -71,11 +80,14 @@ async function  listar() {
         
         const id = document.getElementById("idEdit").value;
         const descricao = document.getElementById("descricaoEdit").value;
+        const unidade = document.getElementById("unidadeEdit").value;
+        const valor_unit = document.getElementById("valorEdit").value;
+        const estoque = document.getElementById("estoqueEdit").value;
       
         await fetch(`${API_URL}/${id}`,{
             method:"PUT",
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({descricao})
+            body: JSON.stringify({nome,uf})
         });
         fecharModal("modalEditar");
         listar();
@@ -86,7 +98,7 @@ async function  listar() {
 
 
     async function deletar(id) {
-        if (!confirm("Deseja excluir esta Forma de Pagamento ?")) return;
+        if (!confirm("Deseja excluir este Produto ?")) return;
         await fetch(`${API_URL}/${id}`,{
             method: "DELETE"
     });
